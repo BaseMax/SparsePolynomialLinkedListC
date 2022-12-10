58bee8dc68992bd25541dd5f58e09ee981eb4225
Max Base <maxbasecode@gmail.com>
2022-12-10 14:03:45 +0100

    adding functions to header

--- SparsePolynomialLinkedList.h
+++ SparsePolynomialLinkedList.h
@@ -15,6 +15,51 @@ typedef struct PolynomialLinkedList {
     struct PolynomialLinkedList *next;
 } PolynomialLinkedList;
 
+/**
+ * @brief Create a Polynomial Linked List object
+ * 
+ * @return PolynomialLinkedList* 
+ */
 PolynomialLinkedList *createPolynomialLinkedList();
+
+/**
+ * @brief Add a Term object
+ * 
+ * @param polynomialLinkedList 
+ * @param term 
+ */
+void addTerm(PolynomialLinkedList *polynomialLinkedList, Term term);
+
+/**
+ * @brief Print a Polynomial Linked List object
+ * 
+ * @param polynomialLinkedList 
+ */
+void printPolynomialLinkedList(PolynomialLinkedList *polynomialLinkedList);
+
+/**
+ * @brief Destroy a Polynomial Linked List object
+ * 
+ * @param polynomialLinkedList 
+ */
+void destroyPolynomialLinkedList(PolynomialLinkedList *polynomialLinkedList);
+
+/**
+ * @brief Add two Polynomial Linked List object
+ * 
+ * @param polynomialLinkedList1 
+ * @param polynomialLinkedList2 
+ * @return PolynomialLinkedList* 
+ */
+PolynomialLinkedList *addPolynomialLinkedList(PolynomialLinkedList *polynomialLinkedList1, PolynomialLinkedList *polynomialLinkedList2);
+
+/**
+ * @brief Subtract two Polynomial Linked List object
+ * 
+ * @param polynomialLinkedList1 
+ * @param polynomialLinkedList2 
+ * @return PolynomialLinkedList* 
+ */
+PolynomialLinkedList *subtractPolynomialLinkedList(PolynomialLinkedList *polynomialLinkedList1, PolynomialLinkedList *polynomialLinkedList2);
 
 #endif
